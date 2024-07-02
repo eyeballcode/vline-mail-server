50a55da51f2f22eb91952d01c796f6722875016e
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-02 13:49:45 +1000

    cover a bit more stuff when standardising the message

--- test/vline-mail-server.test.mjs
+++ test/vline-mail-server.test.mjs
@@ -74,36 +74,45 @@ describe('The V/Line Inform Mail Server', () => {
     })
   })
 
-  describe('The standardisation of names in the message', () => {
+  describe('The standardisation of names and text in the message', () => {
     it('Should replace SCS, SXS and SSS with Southern Cross', () => {
-      expect(VLineMailServer.standardiseNames('SCS to Ballan')).to.equal('Southern Cross to Ballan')
-      expect(VLineMailServer.standardiseNames('SXS to Warragul')).to.equal('Southern Cross to Warragul')
-      expect(VLineMailServer.standardiseNames('SSS to Wandong')).to.equal('Southern Cross to Wandong')
+      expect(VLineMailServer.standardiseMessage('SCS to Ballan')).to.equal('Southern Cross to Ballan')
+      expect(VLineMailServer.standardiseMessage('SXS to Warragul')).to.equal('Southern Cross to Warragul')
+      expect(VLineMailServer.standardiseMessage('SSS to Wandong')).to.equal('Southern Cross to Wandong')
     })
 
     it('Should replace Melbourne with Southern Cross, unless it is North Melbourne', () => {
-      expect(VLineMailServer.standardiseNames('Melbourne to Seymour')).to.equal('Southern Cross to Seymour')
-      expect(VLineMailServer.standardiseNames('Seymour to Melb')).to.equal('Seymour to Southern Cross')
+      expect(VLineMailServer.standardiseMessage('Melbourne to Seymour')).to.equal('Southern Cross to Seymour')
+      expect(VLineMailServer.standardiseMessage('Seymour to Melb')).to.equal('Seymour to Southern Cross')
     })
 
     it('Should not replace Melbourne in North Melbourne', () => {
-      expect(VLineMailServer.standardiseNames('Seymour to North Melbourne')).to.equal('Seymour to North Melbourne')
+      expect(VLineMailServer.standardiseMessage('Seymour to North Melbourne')).to.equal('Seymour to North Melbourne')
     })
 
     it('Should replace Flinders St with an optional colon with just Flinders Street', () => {
-      expect(VLineMailServer.standardiseNames('Flinders St to Traralgon')).to.equal('Flinders Street to Traralgon')
-      expect(VLineMailServer.standardiseNames('Flinders St. to Bairnsdale')).to.equal('Flinders Street to Bairnsdale')
-      expect(VLineMailServer.standardiseNames('Morwell to Flinders Street')).to.equal('Morwell to Flinders Street')
+      expect(VLineMailServer.standardiseMessage('Flinders St to Traralgon')).to.equal('Flinders Street to Traralgon')
+      expect(VLineMailServer.standardiseMessage('Flinders St. to Bairnsdale')).to.equal('Flinders Street to Bairnsdale')
+      expect(VLineMailServer.standardiseMessage('Morwell to Flinders Street')).to.equal('Morwell to Flinders Street')
     })
 
     it('Should replace Jct with Junction', () => {
-      expect(VLineMailServer.standardiseNames('terminate early at Heathcote Jct')).to.equal('terminate early at Heathcote Junction')
-      expect(VLineMailServer.standardiseNames('Heathcote Jct.')).to.equal('Heathcote Junction')
-      expect(VLineMailServer.standardiseNames('Heathcote Junct')).to.equal('Heathcote Junction')
+      expect(VLineMailServer.standardiseMessage('terminate early at Heathcote Jct')).to.equal('terminate early at Heathcote Junction')
+      expect(VLineMailServer.standardiseMessage('Heathcote Jct.')).to.equal('Heathcote Junction')
+      expect(VLineMailServer.standardiseMessage('Heathcote Junct')).to.equal('Heathcote Junction')
     })
 
     it('Should replace Nth with North', () => {
-      expect(VLineMailServer.standardiseNames('originate from Nth Melbourne')).to.equal('originate from North Melbourne')
+      expect(VLineMailServer.standardiseMessage('originate from Nth Melbourne')).to.equal('originate from North Melbourne')
+    })
+
+    it('Should remove the words service and train', () => {
+      expect(VLineMailServer.standardiseMessage('The 12:34 Moe to Morwell service will')).to.equal('The 12:34 Moe to Morwell will')
+      expect(VLineMailServer.standardiseMessage('The 12:34 Southern Cross to Bendigo train will')).to.equal('The 12:34 Southern Cross to Bendigo will')
+    })
+
+    it('Should remove the due to reason', () => {
+      expect(VLineMailServer.standardiseMessage('be delayed due to personal reasons')).to.equal('be delayed')
     })
   })
 })
