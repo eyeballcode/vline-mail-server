85662ac182454adad88c31e01ae9ec2dc5aeedad
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-05 17:17:40 +1000

    remove service identifiers and leave behind the core message

--- test/service-identification.test.mjs
+++ test/service-identification.test.mjs
@@ -3,7 +3,7 @@ import path from 'path'
 import url from 'url'
 
 import { expect } from 'chai'
-import identifyService from '../lib/parsers/identify-service.mjs'
+import identifyService, { removeServiceData } from '../lib/parsers/identify-service.mjs'
 
 import vlineStations from './training-messages/vline-stations.json' assert { type: 'json' }
 
@@ -22,5 +22,23 @@ describe('The service identification function', () => {
       expect(serviceData.origin, text).to.equal(origin)
       expect(serviceData.destination, text).to.equal(destination)
     })
+  })
+})
+
+describe('The service removal function', () => {
+  it('Should remove the matched service text, leaving just the core message', () => {
+    let text = 'The 07:50 Southern Cross to South Geelong will terminate at Lara and no longer run to South Geelong.'
+    let serviceData = identifyService(text, { vlineStations })
+
+    expect(serviceData.matchedText).to.equal('07:50 Southern Cross to South Geelong')
+    expect(removeServiceData(text, serviceData)).to.equal('will terminate at Lara and no longer run to South Geelong.')
+  })
+
+  it('Should also get rid of any further service identifiers that show up', () => {
+    let text = 'The 18:33 Southern Cross to Bairnsdale will terminate at Carnegie and no longer run to Bairnsdale. Customers can board the 18:45 Southern Cross to Traralgon service instead.'
+    let serviceData = identifyService(text, { vlineStations })
+
+    expect(serviceData.matchedText).to.equal('18:33 Southern Cross to Bairnsdale')
+    expect(removeServiceData(text, serviceData)).to.equal('will terminate at Carnegie and no longer run to Bairnsdale. Customers can board the')
   })
 })
