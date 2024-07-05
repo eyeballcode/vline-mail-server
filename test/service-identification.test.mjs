4e1bba70caec9133f99a6bbe5dafac40176fb93d
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-05 16:57:12 +1000

    match closest v/line station - only for origin/dest

--- test/service-identification.test.mjs
+++ test/service-identification.test.mjs
@@ -5,6 +5,8 @@ import url from 'url'
 import { expect } from 'chai'
 import identifyService from '../lib/parsers/identify-service.mjs'
 
+import vlineStations from './training-messages/vline-stations.json' assert { type: 'json' }
+
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -14,7 +16,7 @@ describe('The service identification function', () => {
   it('Correctly identifies the origin, destination and departure time on the test data', () => {
     sampleMessages.forEach(message => {
       let [departureTime, origin, destination, changes, text] = message.split('\t')
-      let serviceData = identifyService(text)
+      let serviceData = identifyService(text, { vlineStations })
 
       expect(serviceData.departureTime, text).to.equal(departureTime)
       expect(serviceData.origin, text).to.equal(origin)
