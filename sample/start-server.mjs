bf422c2f9f6c20813912cc0ce57f20bc08c60e68
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-09 16:18:24 +1000

    actually process messages

--- sample/start-server.mjs
+++ sample/start-server.mjs
@@ -1,11 +1,18 @@
 import VLineMailServer from "../lib/index.mjs"
 
+import vlineStations from '../test/training-messages/vline-stations.json' assert { type: 'json' }
+import lineStops from '../test/training-messages/line-stops.json' assert { type: 'json' }
+
 let server = new VLineMailServer({
   logger: false
-})
+}, { vlineStations, lineStops })
 server.on('error', console.error)
-server.on('cancellation', service => {
-    console.log(service)
+server.on('cancelled', service => {
+  console.log('Received service cancellation! Service:', service)
+})
+
+server.on('non_specific', message => {
+  console.log('Got non specific V/Line message! Contents:', message)
 })
 
 server.setup()
