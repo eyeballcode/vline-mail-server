e6eb8d606f6ed99aa2dd4b24968d52f888229605
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-09 15:20:52 +1000

    move from node-mailin to directly using smtp-server

--- sample/start-server.mjs
+++ sample/start-server.mjs
@@ -1,8 +1,11 @@
 import VLineMailServer from "../lib/index.mjs"
 
-let server = new VLineMailServer()
-server.setup()
+let server = new VLineMailServer({
+  logger: false
+})
+server.on('error', console.error)
 server.on('cancellation', service => {
-  console.log(service)
+    console.log(service)
 })
 
+server.setup()
