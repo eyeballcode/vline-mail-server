0ea550a2be91dcd372021dc6b8fc17d706c23a11
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-09 17:04:04 +1000

    test server receiving an email

--- sample/start-server.mjs
+++ sample/start-server.mjs
@@ -3,9 +3,7 @@ import VLineMailServer from "../lib/index.mjs"
 import vlineStations from '../test/training-messages/vline-stations.json' assert { type: 'json' }
 import lineStops from '../test/training-messages/line-stops.json' assert { type: 'json' }
 
-let server = new VLineMailServer({
-  logger: false
-}, { vlineStations, lineStops })
+let server = new VLineMailServer(null, { vlineStations, lineStops })
 server.on('error', console.error)
 server.on('cancelled', service => {
   console.log('Received service cancellation! Service:', service)
--- test/vline-mail-server.test.mjs
+++ test/vline-mail-server.test.mjs
@@ -3,12 +3,46 @@ import VLineMailServer from '../lib/index.mjs'
 import fs from 'fs/promises'
 import path from 'path'
 import url from 'url'
+import { createReadStream } from 'fs'
+
+import vlineStations from './training-messages/vline-stations.json' assert { type: 'json' }
+import lineStops from './training-messages/line-stops.json' assert { type: 'json' }
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const sampleFullEmail = (await fs.readFile(path.join(__dirname, 'sample-emails', '1555-sss-wvl-amex.eml'))).toString()
+const sampleFullEmail = path.join(__dirname, 'sample-emails', '1555-sss-wvl-amex.eml')
+
+function sampleEmail(server, path) {
+  let stream = createReadStream(path)
+  server.onData(stream, null, () => {})
+}
 
 describe('The V/Line Inform Mail Server', () => {
-  it('Should receive an email and emit an event accordingly')
+  it('Should receive an email and emit an event accordingly', async () => {
+    let server = new VLineMailServer({}, { vlineStations, lineStops })
+    server.on('error', err => { throw err })
+
+    let prom = new Promise((resolve, reject) => {
+      let timeout = setTimeout(() => {
+        reject(new Error('Server did not emit an event upon receiving an email'))
+      }, 200)
+
+      server.on('cancelled', service => {
+        expect(service).to.deep.equal({
+          departureTime: '15:55',
+          origin: 'Southern Cross',
+          destination: 'Wyndham Vale',
+          line: 'Warrnambool',
+          matchedText: '15:55 Southern Cross to Wyndham Vale'
+        })
+
+        clearTimeout(timeout)
+        resolve()
+      })
+    })
+
+    sampleEmail(server, sampleFullEmail)
+    return prom
+  })
 })
