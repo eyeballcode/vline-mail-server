daa2ca72c9b4d637ff281d9b24d55e1636bdc57a
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-12 11:57:29 +1100

    added additional test for badly formatted emails

--- test/vline-mail-server.test.mjs
+++ test/vline-mail-server.test.mjs
@@ -12,6 +12,7 @@ const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const sampleFullEmail = path.join(__dirname, 'sample-emails', '1555-sss-wvl-amex.eml')
+const sampleFullEmail2 = path.join(__dirname, 'sample-emails', '0601-epm-sss-delay-wrong-format.eml')
 
 function sampleEmail(server, path) {
   let stream = createReadStream(path)
@@ -29,20 +30,61 @@ describe('The V/Line Inform Mail Server', () => {
       }, 200)
 
       server.on('cancelled', service => {
-        expect(service).to.deep.equal({
-          departureTime: '15:55',
-          origin: 'Southern Cross',
-          destination: 'Wyndham Vale',
-          line: 'Warrnambool',
-          matchedText: '15:55 Southern Cross to Wyndham Vale'
-        })
-
         clearTimeout(timeout)
-        resolve()
+
+        try {
+          expect(service).to.deep.equal({
+            departureTime: '15:55',
+            origin: 'Southern Cross',
+            destination: 'Wyndham Vale',
+            line: 'Warrnambool',
+            matchedText: '15:55 Southern Cross to Wyndham Vale'
+          })
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
       })
     })
 
     sampleEmail(server, sampleFullEmail)
+    return prom
+  })
+
+  it('Should receive an email with bad formatting and emit an event accordingly', async () => {
+    let server = new VLineMailServer({}, { vlineStations, lineStops })
+    server.on('error', err => { throw err })
+
+    let prom = new Promise((resolve, reject) => {
+      let timeout = setTimeout(() => {
+        reject(new Error('Server did not emit an event upon receiving an email'))
+      }, 200)
+
+      server.on('delayed', (service, serviceData) => {
+        clearTimeout(timeout)
+
+        try {
+          expect(service).to.deep.equal({
+            departureTime: '06:01',
+            origin: 'Epsom',
+            destination: 'Southern Cross',
+            line: 'Echuca',
+            matchedText: '06:01 Epsom to Southern Cross'
+          })
+
+          expect(serviceData).to.deep.equal([{
+            type: 'delay',
+            delayType: 'minutes',
+            value: 20
+          }])
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+
+    sampleEmail(server, sampleFullEmail2)
     return prom
   })
 })
