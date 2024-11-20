84b2dc7d21b06928e67bd59430e6cda1d3a32f96
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-20 17:24:22 +1100

    updated test and index to parse message

--- test/vline-mail-server.test.mjs
+++ test/vline-mail-server.test.mjs
@@ -13,6 +13,7 @@ const __dirname = path.dirname(__filename)
 
 const sampleFullEmail = path.join(__dirname, 'sample-emails', '1555-sss-wvl-amex.eml')
 const sampleFullEmail2 = path.join(__dirname, 'sample-emails', '0601-epm-sss-delay-wrong-format.eml')
+const sampleNonStopHCJ = path.join(__dirname, 'sample-emails', '1236-sss-snh-non-stop-hcj.eml')
 
 function sampleEmail(server, path) {
   let stream = createReadStream(path)
@@ -86,5 +87,42 @@ describe('The V/Line Inform Mail Server', () => {
 
     sampleEmail(server, sampleFullEmail2)
     return prom
+  })
+
+  describe('Testing on sample emails', () => {
+    it('Should detect a service not stopping at a given station', async () => {
+      let server = new VLineMailServer({}, { vlineStations, lineStops })
+      server.on('error', err => { throw err })
+
+      let prom = new Promise((resolve, reject) => {
+        let timeout = setTimeout(() => {
+          reject(new Error('Server did not emit an event upon receiving an email'))
+        }, 200)
+
+        server.on('non_stop', (service, serviceData) => {
+          clearTimeout(timeout)
+
+          try {
+            expect(service).to.deep.equal({
+              departureTime: '12:36',
+              origin: 'Southern Cross',
+              destination: 'Shepparton',
+              line: 'Shepparton',
+              matchedText: '12:36 Southern Cross to Shepparton'
+            })
+
+            expect(serviceData).to.deep.equal([{
+              location: 'Heathcote Junction'
+            }])
+            resolve()
+          } catch (e) {
+            reject(e)
+          }
+        })
+      })
+
+      sampleEmail(server, sampleNonStopHCJ)
+      return prom
+    })
   })
 })
